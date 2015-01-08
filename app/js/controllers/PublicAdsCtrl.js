e87a9e0efaa7712d9c3aa2950363bce5a32431eb
Daniela Stanisheva <dast69@gmail.com>
2015-01-08 15:27:29 +0200

    new pagination

--- app/js/controllers/PublicAdsCtrl.js
+++ app/js/controllers/PublicAdsCtrl.js
@@ -1,13 +1,15 @@
 adsProject.controller('PublicAdsCtrl',['$scope','adsData','filter',function ($scope,adsData,filter) {
-    $scope.totalPages = 0
+    $scope.totalPages = 45
         , $scope.currentPage = 1
         , $scope.numPerPage = 5
         , $scope.maxSize = 3;
 
     $scope.$watch('currentPage + totalPages', function () {
-       // loadPublicAds(filter.getFilterParams());
+        filter.filterByPage($scope.currentPage);
+       loadPublicAds(filter.getParams());
     });
     $scope.ready=false;
+    filter.setPageParams($scope.currentPage,$scope.numPerPage = 5);
     loadPublicAds();
     function loadPublicAds(filterParams) {
         filterParams=filterParams || {};
@@ -15,6 +17,8 @@ adsProject.controller('PublicAdsCtrl',['$scope','adsData','filter',function ($sc
             .$promise
             .then(function (data) {
                 $scope.adsData = data;
+                $scope.totalPages = data.numItems;
+                $scope.numPages = data.numPages;
                 $scope.ready = true;
             });
     }
--- app/js/services/filter.js
+++ app/js/services/filter.js
@@ -9,16 +9,26 @@ adsProject.factory('filter', function () {
         params.townId = town;
     }
 
+    function filterByPage(page){
+        params.startPage=page;
+    }
+
+    function setAdsPerPage(numParPage){
+        params.pageSize=numParPage;
+    }
     function getParams() {
         return params;
     }
-    function setPageParams(pageParams) {
-        return params;
+    function setPageParams(startPage,pageSize) {
+        params.startPage=startPage;
+        params.pageSize=pageSize;
     }
     return {
         filterByCategory: filterByCategory,
         filterByTown: filterByTown,
+        filterByPage:filterByPage,
         getParams: getParams,
+        setAdsPerPage: setAdsPerPage,
         setPageParams:setPageParams
     }
 });
